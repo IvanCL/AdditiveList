6eeaaf5cc47c7b7688d31befe26e7fd5a826cc0e
Ivan Cozar <ivan.cozar90@gmail.com>
2019-07-09 23:42:52 +0200

    kick off

---
+++ app/src/main/java/com/icl/additivelist/models/Additive.kt
@@ -0,0 +1,3 @@
+package com.icl.additivelist.models
+
+data class Additive(val id :String, val numb: String, val name: String, val description : String, val func : String, val sideEffects : String, val origin :String)
---
+++ app/src/main/java/com/icl/additivelist/usescase/additives/FindAdditivesActivity.kt
@@ -0,0 +1,14 @@
+package com.icl.additivelist.usescase.additives
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.icl.additivelist.R
+
+class FindAdditivesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_find_additives)
+    }
+}
+0
