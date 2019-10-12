039a2263b0111eca45c00618e744577071a68bd5
Ivan Cozar <ivan.cozar90@gmail.com>
2019-10-12 02:34:17 +0200

    splash 100%, main with functionality

--- app/src/main/java/com/icl/additivelist/globals/Constants.kt
+++ app/src/main/java/com/icl/additivelist/globals/Constants.kt
@@ -2,4 +2,7 @@ package com.icl.additivelist.globals
 
 const val GLOBAL_ADDITIVES = "Aditivos"
 const val GLOBAL_PRODUCTS = "Productos"
+const val ADDITIVES ="ADDITIVES"
+const val DAYS_FROM_ADDITIVES_UPDATE ="DAYS_FROM_ADDITIVES_UPDATE"
+const val PRODUCTS = "PRODUCTS"
 
--- app/src/main/java/com/icl/additivelist/usescase/main/MainActivity.kt
+++ app/src/main/java/com/icl/additivelist/usescase/main/MainActivity.kt
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.icl.additivelist.R
 import com.icl.additivelist.usescase.additives.AdditivesActivity
+import com.icl.additivelist.usescase.products.ProductsActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,7 +14,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var intent = Intent(this@MainActivity, AdditivesActivity::class.java)
-        startActivity(intent)
+        clickOnAdditives()
+        clickOnProducts()
+    }
+
+    private fun clickOnAdditives() {
+        additivesTxt.setOnClickListener {
+            var intent = Intent(this.applicationContext, AdditivesActivity::class.java)
+            startActivity(intent)
+        }
+        additivesImg.setOnClickListener {
+            var intent = Intent(this.applicationContext, AdditivesActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun clickOnProducts() {
+        productsImg.setOnClickListener {
+            var intent = Intent(this.applicationContext, ProductsActivity::class.java)
+            startActivity(intent)
+        }
+        productsTxt.setOnClickListener {
+            var intent = Intent(this.applicationContext, ProductsActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
